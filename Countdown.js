e838bcf86bd8ba813898de62898880eb568016d0
Kieran <kieran.milligan@gmail.com>
2021-01-06 18:28:16 -0600

    deploying w/ ghpages

---
+++ Countdown.js
@@ -0,0 +1,29 @@
+var freedomRings = new Date("Jan 20, 2021 12:00:00").getTime();
+
+console.log(countDown);
+
+var countDown = setInterval(() => {
+  var now = new Date().getTime();
+  console.log(now);
+  var deathMarch = freedomRings - now;
+
+  var days = Math.floor(deathMarch / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((deathMarch % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((deathMarch % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((deathMarch % (1000 * 60)) / 1000);
+
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  console.log(days + ":" + hours + ":" + minutes + ":" + seconds);
+
+  document.getElementById("timer").innerHTML = days + ":" + hours + ":" + minutes + ":" + seconds;
+  document.getElementById("text").innerHTML = "Trump will be gone in:";
+
+  if (deathMarch < 0) {
+    clearInterval(countDown);
+    document.getElementById("text").innerHTML = "America has Rejected Authoritarianism";
+    document.getElementById("timer").innerHTML = "And Just Like That, He's Gone...";
+  }
+}, 1000)
